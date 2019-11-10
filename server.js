b19ac97a3503c4e07a8247fd0b817f3983fc4925
Sam <callkaff@gmail.com>
2019-11-10 11:30:11 -0800

    successfully import contact from cluster mongoos

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
 
 var app = express();
+require('./models/db');
+
+const Contact = require ('./models/Contact')
+const parser  = require('body-parser');
+const path = require("path");
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -9,5 +14,18 @@ app.set('view engine', 'handlebars');
 app.get('/', function (req, res) {
     res.render('home');
 });
+
+app.get('/contacts',async function(req,res){
+
+    const items = await Contact.list(); // query to database for params
+
+    res.render("contact",{
+      items,
+
+    });
+
+
+});
+
 
 app.listen(3000);
